6e09b1fef7277e1f5a49e1d58fa1b2245025b312
ccpretorius <84196590+ccpretorius@users.noreply.github.com>
2024-02-04 15:37:19 +0100

    setting timers and managing state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,29 +3,16 @@ import { useState, useRef } from "react";
 export default function Player() {
   const playerName = useRef();
   const [enteredPlayerName, setEnteredPlayerName] = useState(null);
-  // const [submitted, setSubmitted] = useState(false);
-
-  // function handleChange(e) {
-  //   setSubmitted(false);
-  //   // setEnteredPlayerName((prevState) => e.target.value); // this is only required when you need to update state based on previous state
-  //   setEnteredPlayerName(e.target.value);
-  //   console.log(enteredPlayerName);
-  // }
 
   function handleClick() {
-    //setSubmitted(true);
     setEnteredPlayerName(playerName.current.value);
+    playerName.current.value = "";
   }
 
   return (
     <section id="player">
-      {/* <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2> */}
-      {/* This is the regular manner for refs: */}
-      {/* <h2>Welcome {enteredPlayerName ? enteredPlayerName : "unknown entity"}</h2> */}
-      {/* shortcut: */}
       <h2>Welcome {enteredPlayerName ?? "unknown entity"}</h2>
       <p>
-        {/* GET RID OF THIS FOR REFS inside the input element: onChange={handleChange} and value={enteredPlayerName}*/}
         <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
